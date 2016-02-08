85cd9bf8ee1f4799205f386eb7d1cba9fb179164
Elie Viviant <elie.viviant@ung.edu>
2016-02-07 22:45:42 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## I am sorry but I wasn't able to push my file so I finally copied and pasted as the deadline approach
+## I could send my file from my computer with the timestamp in needed.
+## function 
+
+## create a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv = NULL
+  set = function(y){
+    
+    x <<- y  
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## Check to see the inverse matrix is already in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  inv = x$getinv()
+  
+  if(!is.null(inv)){
+    
+    message("getting cached data")
+    return(inv)
+  }
+  
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  x$setinv(inv)
+  
+  return(inv)
+  
 }
+##> x<-rbind(c(1, -2), c(-2, 1))
+##> m <- makeCacheMatrix(x)
+##> m$get()
+##[,1] [,2]
+##[1,]    1   -2
+##[2,]   -2    1
+##> cacheSolve(m)
+##[,1]       [,2]
+##[1,] -0.3333333 -0.6666667
+##[2,] -0.6666667 -0.3333333
+##> cacheSolve(m)
+##getting cached data
+##[,1]       [,2]
+##[1,] -0.3333333 -0.6666667
+##[2,] -0.6666667 -0.3333333
+## not sure if it worked would have to check compute time but it printed getting cached data
